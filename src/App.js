cc1c9955e4ec1a9904020c02fd31c5c9b323f5d4
Carlos Henrique <chtsca2001@gmail.com>
2025-05-19 18:30:21 -0300

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import Navbar from './components/user/Navbar';
+import { BsFillSunFill } from 'react-icons/bs';
 
 export default function App() {
     return (
-        <div className='text-red-500 font-bold text-2xl'>App</div>
+        <div className='bg-secondary'>
+            <div className='max-w-screen-xl mx-auto p-2'>
+                <div className='flex justify-between items-center'>
+                    <img src='./logo.png' alt='' className='h-10' />
+
+                    <ul className='flex items-center space-x-4'>
+                        <li>
+                            <button className='bg-dark-subtle p-1 rounded'>
+                                <BsFillSunFill className='text-secondary' size={24} />
+                            </button>
+                        </li>
+                        <li>
+                            <input
+                                type='text'
+                                className='border-2 border-dark-subtle p-1 rounded bg-transparent 
+                                text-xl outline-none focus:border-white transition text-white'
+                                placeholder='Search...'
+                            />
+                        </li>
+                        <li className='text-white font-semibold text-lg'>Login</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     );
 };
