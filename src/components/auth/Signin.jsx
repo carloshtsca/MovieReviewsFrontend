e7a1c0da576104146cde6bf168a7035363e8f5ce
Carlos Henrique <chtsca2001@gmail.com>
2025-05-19 20:44:47 -0300

    Add Signin Form

---
+++ src/components/auth/Signin.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Container from '../Container'
+import Title from '../form/Title'
+import FormInput from '../form/FormInput'
+import Submit from '../form/Submit'
+
+export default function Signin() {
+    return (
+        <div className='fixed inset-0 bg-primary -z-10 flex justify-center items-center'>
+            <Container>
+                <form className='bg-secondary rounded p-6 w-72 space-y-6'>
+                    <Title children='Sign In' />
+                    <FormInput label='Email' placeholder='john@email.com' name='email' />
+                    <FormInput label='Password' placeholder='***********' name='password' />
+                    <Submit value='Sign in' />
+
+                    <div className='flex justify-between'>
+                        <a className='text-dark-subtle hover:text-white transition' href='#'>Forget password</a>
+                        <a className='text-dark-subtle hover:text-white transition' href='#'>Sign up</a>
+                    </div>
+                </form>
+            </Container>
+        </div>
+    )
+}
