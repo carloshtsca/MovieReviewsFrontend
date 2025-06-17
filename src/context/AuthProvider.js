968729309d818cf488f849498351f90811f5f293
Carlos Henrique <chtsca2001@gmail.com>
2025-06-17 15:40:10 -0300

    Fixing Navigation Issue

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react'
 import { getIsAuth, signInUser } from '../api/auth';
 import { useNotification } from '../hooks';
+import { useNavigate } from 'react-router-dom';
 
 export const AuthContext = createContext();
 
@@ -15,6 +16,8 @@ export default function AuthProvider({ children }) {
     const [authInfo, setAuthInfo] = useState({ ...defaultAuthInfo });
     const { updateNotification } = useNotification();
 
+    const navigate = useNavigate();
+
     const handleLogin = async (email, password, navigate) => {
         setAuthInfo({ ...authInfo, isPending: true });
 
@@ -24,6 +27,8 @@ export default function AuthProvider({ children }) {
             updateNotification('error', error);
             return setAuthInfo({ ...authInfo, isPending: false, error });
         }
+
+        navigate('/', { replace: true });
 
         setAuthInfo({
             profile: { ...user },
