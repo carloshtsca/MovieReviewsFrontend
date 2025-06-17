aab84b6a88e51fc837df48e280735d419c3ea3a4
Carlos Henrique <chtsca2001@gmail.com>
2025-06-17 18:40:50 -0300

    Marking Admin Nav Visible Everywhere

--- src/navigator/AdminNavigator.jsx
+++ src/navigator/AdminNavigator.jsx
@@ -4,14 +4,21 @@ import Dashboard from '../components/admin/Dashboard';
 import Movies from '../components/admin/Movies';
 import Actors from '../components/admin/Actors';
 import NotFound from '../components/NotFound';
+import Sidebar from '../components/admin/Sidebar';
 
 export default function AdminNavigator() {
     return (
-        <Routes>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/movies' element={<Movies />} />
-            <Route path='/actors' element={<Actors />} />
-            <Route path='*' element={<NotFound />} />
-        </Routes>
+        <div className='flex'>
+            <Sidebar />
+
+            <div className="flex-1 p-2 max-w-screen-xl">
+                <Routes>
+                    <Route path='/' element={<Dashboard />} />
+                    <Route path='/movies' element={<Movies />} />
+                    <Route path='/actors' element={<Actors />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+            </div>
+        </div>
     );
 };
