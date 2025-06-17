790e324843ec2f2e5ebabf8705ad9c6353bee2b6
Carlos Henrique <chtsca2001@gmail.com>
2025-06-17 10:57:29 -0300

    Sending Admin Role From Frontend

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ import EmailVerification from './components/auth/EmailVerification';
 import ForgetPassword from './components/auth/ForgetPassword';
 import ConfirmPassword from './components/auth/ConfirmPassword';
 import NotFound from './components/NotFound';
+import { useAuth } from './hooks';
 
 export default function App() {
+    const { authInfo } = useAuth();
+    console.log(authInfo);
+
     return (
         <div>
             <Navbar />
