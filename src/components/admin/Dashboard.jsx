c44353d3c84da483616f4c1c0fe174d001eba68a
Carlos Henrique <chtsca2001@gmail.com>
2025-06-18 13:07:35 -0300

    Toggling Options Modal

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 
 export default function Dashboard() {
+    const [showOptions, setShowOptions] = useState(false);
+
     return (
         <div className="flex items-center justify-between relative">
             <input
@@ -12,6 +14,7 @@ export default function Dashboard() {
                 placeholder="Search Movies..."
             />
             <button
+                onClick={() => setShowOptions(!showOptions)}
                 className="flex items-center space-x-2 border-secondary 
                 hover:border-primary text-secondary hover:opacity-80 
                 transition font-semibold border-2 rounded text-lg px-3
@@ -21,12 +24,14 @@ export default function Dashboard() {
                 <AiOutlinePlus />
             </button>
 
-            <CreateOptions />
+            <CreateOptions visible={showOptions} />
         </div>
     );
 };
 
-const CreateOptions = () => {
+const CreateOptions = ({ visible }) => {
+    if (!visible) return null;
+    
     return (
         <div className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded">
             <Option>Add Movie</Option>
