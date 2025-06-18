83626e1f41ad66c29317f1bb6f0006f6f629cfbf
Carlos Henrique <chtsca2001@gmail.com>
2025-06-18 13:46:18 -0300

    Adding Animation to Options Modal

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -31,9 +31,9 @@ export default function Dashboard() {
 
 const CreateOptions = ({ visible }) => {
     if (!visible) return null;
-    
+
     return (
-        <div className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded">
+        <div className="absolute animate-scale right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded">
             <Option>Add Movie</Option>
             <Option>Add Actor</Option>
         </div>
