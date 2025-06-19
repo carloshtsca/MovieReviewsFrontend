18867d4e9e70a049dcc391907c617e6b23398577
Carlos Henrique <chtsca2001@gmail.com>
2025-06-19 17:12:15 -0300

    Rendering React Drag Drop Files

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,4 +1,5 @@
 export const isValidEmail = (email) => {
+    // eslint-disable-next-line no-useless-escape
     const isValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return isValid.test(email);
 }
