326e97474a165e5594701d44536ed5d346773a1a
Carlos Henrique <chtsca2001@gmail.com>
2025-07-01 14:13:32 -0300

    Moving Selection Up & Down

--- src/components/LiveSearch.jsx
+++ src/components/LiveSearch.jsx
@@ -42,6 +42,7 @@ export const results = [
 
 export default function LiveSearch() {
     const [displaySearch, setDisplaySearch] = useState(false);
+    const [focusedIndex, setFocusedIndex] = useState(-1);
 
     const handleOnFocus = () => {
         if (results.length) setDisplaySearch(true);
@@ -49,6 +50,22 @@ export default function LiveSearch() {
 
     const handleOnBlur = () => {
         setDisplaySearch(false);
+    }
+
+    const handleKeyDown = ({ key }) => {
+        let nextCount;
+        const keys = ['ArrowDown', 'ArrowUp', 'Enter', 'Escape'];
+        if (!keys.includes(key)) return;
+
+        // move selection up and down
+        if (key === 'ArrowDown') {
+            nextCount = focusedIndex + 1;
+        }
+        if (key === 'ArrowUp') {
+            nextCount = focusedIndex - 1
+        }
+
+        setFocusedIndex(nextCount);
     }
 
     return (
@@ -59,26 +76,31 @@ export default function LiveSearch() {
                 placeholder='Search Profile'
                 onFocus={handleOnFocus}
                 onBlur={handleOnBlur}
+                onKeyDown={handleKeyDown}
             />
-            <SearchResults visible={displaySearch} results={results} />
+            <SearchResults
+                focusedIndex={focusedIndex}
+                visible={displaySearch}
+                results={results}
+            />
         </div>
     );
 };
 
-const SearchResults = ({ visible, results = [] }) => {
+const SearchResults = ({ visible, results = [], focusedIndex }) => {
     if (!visible) return null;
 
     return (
         <div className='absolute right-0 left-0 top-10 bg-white 
             dark:bg-secondary shadow-md p-2 max-h-64 space-y-2 mt-1
             overflow-auto custom-scroll-bar'>
-            {results.map(({ id, name, avatar }) => {
+            {results.map(({ id, name, avatar }, index) => {
                 return (
                     <div
                         key={id}
-                        className='cursor-pointer rounded overflow-hidden 
-                            dark:hover:bg-dark-subtle hover:bg-light-subtle 
-                            transition flex space-x-2'
+                        className={`${index === focusedIndex ? 'dark:bg-dark-subtle bg-light-subtle' : ''} cursor-pointer rounded overflow-hidden 
+                        dark:hover:bg-dark-subtle hover:bg-light-subtle 
+                        transition flex space-x-2`}
                     >
                         <img src={avatar} alt={name} className='w-16 h-16 rounded object-cover' />
                         <p className='dark:text-white font-semibold'>{name}</p>
