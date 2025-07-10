25048a901244d960e261e53c789c3da538b953dd
Carlos Henrique <chtsca2001@gmail.com>
2025-07-10 16:05:09 -0300

    Reusable Modal Container

--- src/components/admin/MovieForm.jsx
+++ src/components/admin/MovieForm.jsx
@@ -4,6 +4,7 @@ import LiveSearch from '../LiveSearch';
 import { commonInputClasses } from '../../utils/theme';
 import Submit from '../form/Submit';
 import { useNotification } from '../../hooks';
+import ModalContainer from '../modals/ModalContainer';
 
 export const results = [
     {
@@ -61,6 +62,7 @@ const defaultMovieInfo = {
 
 export default function MovieForm() {
     const [movieInfo, setMovieInfo] = useState({ ...defaultMovieInfo });
+    const [showModal, setShowModal] = useState(false);
 
     const { updateNotification } = useNotification();
 
@@ -104,71 +106,83 @@ export default function MovieForm() {
     const { title, storyLine, director, writers } = movieInfo;
 
     return (
-        <form onSubmit={handleSubmit} className='flex space-x-3'>
-            <div className='w-[70%] h-5 space-y-5'>
-                <div>
-                    <Label htmlFor='title'>Title</Label>
-                    <input
-                        id='title'
-                        name='title'
-                        type='text'
-                        value={title}
-                        onChange={handleChange}
-                        className={`${commonInputClasses} border-b-2 font-semibold text-xl`}
-                        placeholder='Titanic'
-                    />
-                </div>
-
-                <div>
-                    <Label htmlFor="storyLine">Story line</Label>
-                    <textarea
-                        id='storyLine'
-                        name='storyLine'
-                        value={storyLine}
-                        onChange={handleChange}
-                        className={`${commonInputClasses} border-b-2 resize-none h-24`}
-                        placeholder='Movie story line...'
-                    ></textarea>
-                </div>
-
-                <div>
-                    <Label htmlFor='tags'>Tags</Label>
-                    <TagsInput name='tags' onChange={updateTags} />
-                </div>
-
-                <div>
-                    <Label htmlFor='director'>Director</Label>
-                    <LiveSearch
-                        name='director'
-                        value={director.name}
-                        placeholder="Search profile"
-                        results={results}
-                        renderItem={renderItem}
-                        onSelect={updateDirector}
-                    />
-                </div>
-
-                <div>
-                    <div className="flex justify-between">
-                        <LabelWithBadge badge={writers.length} htmlFor='writers'>
-                            Writers
-                        </LabelWithBadge>
-                        <button className='dark:text-white text-primary hover:underline 
-                        transition'>View All</button>
+        <>
+            <form onSubmit={handleSubmit} className='flex space-x-3'>
+                <div className='w-[70%] h-5 space-y-5'>
+                    <div>
+                        <Label htmlFor='title'>Title</Label>
+                        <input
+                            id='title'
+                            name='title'
+                            type='text'
+                            value={title}
+                            onChange={handleChange}
+                            className={`${commonInputClasses} border-b-2 font-semibold text-xl`}
+                            placeholder='Titanic'
+                        />
                     </div>
-                    <LiveSearch
-                        name='writers'
-                        placeholder="Search profile"
-                        results={results}
-                        renderItem={renderItem}
-                        onSelect={updateWriters}
-                    />
-                </div>
 
-                <Submit value='Upload' />
-            </div>
-            <div className='w-[30%] h-5 bg-blue-400'></div>
-        </form>
+                    <div>
+                        <Label htmlFor="storyLine">Story line</Label>
+                        <textarea
+                            id='storyLine'
+                            name='storyLine'
+                            value={storyLine}
+                            onChange={handleChange}
+                            className={`${commonInputClasses} border-b-2 resize-none h-24`}
+                            placeholder='Movie story line...'
+                        ></textarea>
+                    </div>
+
+                    <div>
+                        <Label htmlFor='tags'>Tags</Label>
+                        <TagsInput name='tags' onChange={updateTags} />
+                    </div>
+
+                    <div>
+                        <Label htmlFor='director'>Director</Label>
+                        <LiveSearch
+                            name='director'
+                            value={director.name}
+                            placeholder="Search profile"
+                            results={results}
+                            renderItem={renderItem}
+                            onSelect={updateDirector}
+                        />
+                    </div>
+
+                    <div>
+                        <div className="flex justify-between">
+                            <LabelWithBadge badge={writers.length} htmlFor='writers'>
+                                Writers
+                            </LabelWithBadge>
+                            <button
+                                onClick={() => setShowModal(true)}
+                                className='dark:text-white text-primary hover:underline 
+                                transition'
+                            >
+                                View All
+                            </button>
+                        </div>
+                        <LiveSearch
+                            name='writers'
+                            placeholder="Search profile"
+                            results={results}
+                            renderItem={renderItem}
+                            onSelect={updateWriters}
+                        />
+                    </div>
+
+                    <Submit value='Upload' />
+                </div>
+                <div className='w-[30%] h-5 bg-blue-400'></div>
+            </form>
+            <ModalContainer onClose={() => setShowModal(false)} visible={showModal}>
+                <div className="p-20 bg-red-200">
+
+                </div>
+            </ModalContainer>
+        </>
     );
 };
 
