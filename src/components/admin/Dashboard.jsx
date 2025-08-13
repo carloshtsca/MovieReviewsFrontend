91de61492a7ece142d10d0aa07c69ba12646ae0a
Carlos Henrique <chtsca2001@gmail.com>
2025-08-13 11:10:13 -0300

    Creating Info Container

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,3 +1,10 @@
 export default function Dashboard() {
-    return null
+    return (
+        <div className="bg-white shadow dark:bg-secondary p-5 rounded">
+            <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
+                Total Uploads
+            </h1>
+            <p className="text-2xl mb-2 text-primary dark:text-white">100</p>
+        </div>
+    );
 };
