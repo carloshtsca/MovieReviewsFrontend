4f7a17be8a65a90813afa5517fd46dadc0b31241
Carlos Henrique <chtsca2001@gmail.com>
2025-08-13 14:35:28 -0300

    Rendering Overlay on Actor Profile

--- src/components/admin/Actors.jsx
+++ src/components/admin/Actors.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+import { BsPencilSquare, BsTrash } from "react-icons/bs";
+
 export default function Actors() {
+    const [showOptions, setShowOptions] = useState(false);
+
     return (
         <div className="grid grid-cols-4 gap-3 my-5">
             <div className="bg-white shadow dark:shadow dark:bg-secondary p-5 rounded h-20 overflow-hidden">
-                <div className="flex cursor-pointer">
+                <div className="flex cursor-pointer relative">
                     <img
                         className='w-20 aspect-square object-cover'
                         src="https://images.unsplash.com/photo-1735029660539-59df2bded95b?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -13,6 +18,20 @@ export default function Actors() {
                         <h1 className="text-xl text-primary dark:text-white font-semibold">John Doe</h1>
                         <p className="text-primary dark:text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                     </div>
+
+                    {showOptions
+                        ?
+                        <div className="absolute inset-0 bg-primary bg-opacity-25 backdrop-blur-sm">
+                            <button type="button">
+                                <BsTrash />
+                            </button>
+                            <button type="button">
+                                <BsPencilSquare />
+                            </button>
+                        </div>
+                        :
+                        null
+                    }
                 </div>
             </div>
         </div>
