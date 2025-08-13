44ffc95ada148e9704d0b79d5ed81e6b399be043
Carlos Henrique <chtsca2001@gmail.com>
2025-08-13 12:52:52 -0300

    Latest Uploads Movie Card

--- src/components/LatestUploads.jsx
+++ src/components/LatestUploads.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { BsTrash, BsPencilSquare, BsBoxArrowUpRight } from "react-icons/bs";
 
 export default function LatestUploads() {
     return (
@@ -14,6 +14,48 @@ export default function LatestUploads() {
 
 const MovieListItem = () => {
     return (
-        <p>Movie list </p>
+        <table className='w-full border-b'>
+            <tbody>
+                <tr>
+                    <td>
+                        <div className="w-24">
+                            <img
+                                className='w-full aspect-video'
+                                src="https://images.unsplash.com/photo-1735029660539-59df2bded95b?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                alt="poster-img"
+                            />
+                        </div>
+                    </td>
+
+                    <td className="w-full pl-5">
+                        <div>
+                            <h1 className='text-lg font-semibold text-primary dark:text-white'>
+                                Lorem ipsum dolor sit amet.
+                            </h1>
+                            <div className='space-x-1'>
+                                <span className='font-semibold text-primary dark:text-white text-xs'>
+                                    Action
+                                </span>
+                                <span className='font-semibold text-primary dark:text-white text-xs'>
+                                    Drama
+                                </span>
+                            </div>
+                        </div>
+                    </td>
+
+                    <td className="px-5">
+                        <p className='text-primary dark:text-white'>public</p>
+                    </td>
+
+                    <td>
+                        <div className='flex items-center space-x-3 text-primary dark:text-white text-lg'>
+                            <button type='button'><BsTrash /></button>
+                            <button type='button'><BsPencilSquare /></button>
+                            <button type='button'><BsBoxArrowUpRight /></button>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     );
 };
