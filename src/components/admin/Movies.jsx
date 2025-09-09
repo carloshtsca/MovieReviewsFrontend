8ff0136f2dba58085821d391054f7f928a67da06
Carlos Henrique <chtsca2001@gmail.com>
2025-09-09 12:10:21 -0300

    Making Movies Component Ready To Render Movies

--- src/components/admin/Movies.jsx
+++ src/components/admin/Movies.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { useState } from 'react'
+import MovieListItem from '../MovieListItem';
 
 export default function Movies() {
+    const [movies, setMovies] = useState([]);
+
     return (
-        <div>Movies</div>
+        <div className='space-y-3 p-5'>
+            {movies.map(movie => {
+                return <MovieListItem movie={movie} />
+            })}
+        </div>
     );
 };
