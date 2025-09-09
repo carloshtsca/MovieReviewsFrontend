6244213561e64e74c6a766803b02810562f6ed59
Carlos Henrique <chtsca2001@gmail.com>
2025-09-09 18:02:16 -0300

    Moving to The Next Movies Page

--- src/components/admin/Movies.jsx
+++ src/components/admin/Movies.jsx
@@ -2,19 +2,40 @@ import { useEffect, useState } from 'react'
 import MovieListItem from '../MovieListItem';
 import { useNotification } from '../../hooks';
 import { getMovies } from '../../api/movie';
+import NextAndPrevButton from '../NextAndPrevButton';
 
-const limit = 10;
+const limit = 1;
 let currentPageNo = 0;
 
 export default function Movies() {
     const [movies, setMovies] = useState([]);
+    const [reachedToEnd, setReachedToEnd] = useState(false);
 
     const { updateNotification } = useNotification();
 
     const fetchMovies = async (pageNo) => {
         const { error, movies } = await getMovies(pageNo, limit);
         if (error) updateNotification('error', error);
+
+        if (!movies.length) {
+            currentPageNo = pageNo - 1;
+            return setReachedToEnd(true);
+        };
+        
+        setReachedToEnd(false);
         setMovies([...movies]);
+    };
+
+    const handleOnNextClick = () => {
+        if (reachedToEnd) return;
+        currentPageNo += 1;
+        fetchMovies(currentPageNo);
+    };
+
+    const handleOnPrevClick = () => {
+        if (currentPageNo <= 0) return;
+        currentPageNo -= 1;
+        fetchMovies(currentPageNo);
     };
 
     useEffect(() => {
@@ -26,6 +47,12 @@ export default function Movies() {
             {movies.map(movie => {
                 return <MovieListItem movie={movie} />
             })}
+
+            <NextAndPrevButton
+                className="mt-5"
+                onNextClick={handleOnNextClick}
+                onPrevClick={handleOnPrevClick}
+            />
         </div>
     );
 };
