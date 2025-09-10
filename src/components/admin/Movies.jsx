aa47ccb3e47435da5e463b631afcf47335ace9dd
Carlos Henrique <chtsca2001@gmail.com>
2025-09-10 09:51:47 -0300

    Unterstanding What We Did So Far

--- src/components/admin/Movies.jsx
+++ src/components/admin/Movies.jsx
@@ -45,7 +45,7 @@ export default function Movies() {
     return (
         <div className='space-y-3 p-5'>
             {movies.map(movie => {
-                return <MovieListItem movie={movie} />
+                return <MovieListItem key={movie.id} movie={movie} />
             })}
 
             <NextAndPrevButton
