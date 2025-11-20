2bd6f3fe76a13e94751450d7071b39884c4d31c0
Carlos Henrique <chtsca2001@gmail.com>
2025-11-20 17:35:21 -0300

    Movie Delete From Dashboard

--- src/components/LatestUploads.jsx
+++ src/components/LatestUploads.jsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from "react";
 import { useNotification } from "../hooks";
 import MovieListItem from "./MovieListItem";
-import { getMovies } from "../api/movie";
+import { deleteMovie, getMovieForUpdate, getMovies } from "../api/movie";
+import ConfirmModal from "./modals/ConfirmModal";
+import UpdateMovie from "./modals/UpdateMovie";
 
 const pageNo = 0;
 const limit = 5;
 
 export default function LatestUploads() {
     const [movies, setMovies] = useState([]);
+    const [busy, setBusy] = useState(false);
+    const [showUpdateModal, setShowUpdateModal] = useState(false);
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
+    const [selectedMovie, setSelectedMovie] = useState(null);
+
     const { updateNotification } = useNotification();
 
     const fetchLatestUploads = async () => {
@@ -16,21 +23,73 @@ export default function LatestUploads() {
         setMovies([...movies]);
     };
 
+    const handleOnEditClick = async ({ id }) => {
+        const { movie, error } = await getMovieForUpdate(id);
+        if (error) return updateNotification('error', error);
+        setSelectedMovie(movie);
+        setShowUpdateModal(true);
+    };
+
+    const handleOnDeleteClick = async (movie) => {
+        setSelectedMovie(movie);
+        setShowConfirmModal(true);
+    };
+
+    const handleOnDeleteConfirm = async () => {
+        setBusy(true);
+        const { error, message } = await deleteMovie(selectedMovie.id);
+        setBusy(false);
+        if (error) return updateNotification('error', error);
+        updateNotification('success', message);
+        hideConfirmModal();
+        fetchLatestUploads();
+    };
+
+    const handleOnUpdate = (movie) => {
+        const updatedMovies = movies.map(m => {
+            if (m.id === movie.id) return movie;
+            return m
+        });
+
+        setMovies([...updatedMovies]);
+    };
+
+    const hideUpdateForm = () => setShowUpdateModal(false);
+    const hideConfirmModal = () => setShowConfirmModal(false);
+
     useEffect(() => {
         fetchLatestUploads();
     }, []);
 
     return (
-        <div className="bg-white shadow dark:shadow dark:bg-secondary p-5 rounded col-span-2">
-            <h1 className='font-semibold text-2xl mb-2 text-primary dark:text-white'>
-                Recent Uploads
-            </h1>
+        <>
+            <div className="bg-white shadow dark:shadow dark:bg-secondary p-5 rounded col-span-2">
+                <h1 className='font-semibold text-2xl mb-2 text-primary dark:text-white'>
+                    Recent Uploads
+                </h1>
 
-            <div className='space-y-3'>
-                {movies.map((movie) => {
-                    return <MovieListItem movie={movie} key={movie.id} />;
-                })}
+                <div className='space-y-3'>
+                    {movies.map((movie) => {
+                        return <MovieListItem
+                            movie={movie}
+                            key={movie.id}
+                            onEditClick={() => handleOnEditClick(movie)}
+                            onDeleteClick={() => handleOnDeleteClick(movie)}
+                        />;
+                    })}
+                </div>
             </div>
-        </div>
+
+            <ConfirmModal
+                visible={showConfirmModal}
+                onConfirm={handleOnDeleteConfirm}
+                onCancel={hideConfirmModal}
+                title='Are you sure?'
+                subtitle={`This action will remove this movie '${selectedMovie?.title}' permanently!`}
+                busy={busy}
+            />
+
+            
+        </>
     );
 };
--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -3,7 +3,7 @@ import LatestUploads from "../LatestUploads";
 
 export default function Dashboard() {
     return (
-        <div className="grid grid-cols-3 gap-5 my-5">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-5 p-5">
             <AppInfoBox title='Total Uploads' subTitle='100' />
             <AppInfoBox title='Total Reviews' subTitle='1,500' />
             <AppInfoBox title='Total Users' subTitle='200' />
