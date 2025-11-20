177dd73c431ed7f876bd1a1f144995ec67c6b211
Carlos Henrique <chtsca2001@gmail.com>
2025-11-20 15:39:13 -0300

    Fetching Latest Uploads For Admin

--- src/components/LatestUploads.jsx
+++ src/components/LatestUploads.jsx
@@ -1,20 +1,36 @@
+import { useEffect, useState } from "react";
+import { useNotification } from "../hooks";
 import MovieListItem from "./MovieListItem";
+import { getMovies } from "../api/movie";
+
+const pageNo = 0;
+const limit = 5;
 
 export default function LatestUploads() {
+    const [movies, setMovies] = useState([]);
+    const { updateNotification } = useNotification();
+
+    const fetchLatestUploads = async () => {
+        const { error, movies } = await getMovies(pageNo, limit);
+        if (error) return updateNotification('error', error);
+        setMovies([...movies]);
+    };
+
+    useEffect(() => {
+        fetchLatestUploads();
+    }, []);
+
     return (
         <div className="bg-white shadow dark:shadow dark:bg-secondary p-5 rounded col-span-2">
             <h1 className='font-semibold text-2xl mb-2 text-primary dark:text-white'>
                 Recent Uploads
             </h1>
 
-            <MovieListItem
-                movie={{
-                    poster: 'https://images.unsplash.com/photo-1735029660539-59df2bded95b?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                    title: 'Lorem ipsum dolor sit amet',
-                    status: 'public',
-                    genres: ['Action', 'Comedy'],
-                }}
-            />
+            <div className='space-y-3'>
+                {movies.map((movie) => {
+                    return <MovieListItem movie={movie} key={movie.id} />;
+                })}
+            </div>
         </div>
     );
 };
