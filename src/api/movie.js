0ade30fa30411b8d1e29dd4bb24845982b0866d3
Carlos Henrique <chtsca2001@gmail.com>
2026-03-02 08:37:55 -0300

    Fetching Top Rated Movies

--- src/api/movie.js
+++ src/api/movie.js
@@ -105,3 +105,15 @@ export const searchMovieForAdmin = async (title) => {
         return catchError(error);
     }
 };
+
+export const getTopRatedMovies = async (type) => {
+    try {
+        let endpoint = '/movie/top-rated';
+        if (type) endpoint = endpoint + '?type=' + type;
+        
+        const { data } = await client(endpoint);
+        return data;
+    } catch (error) {
+        return catchError(error);
+    }
+};
--- src/components/user/TopRatedMovies.jsx
+++ src/components/user/TopRatedMovies.jsx
@@ -1,14 +1,27 @@
+import { useState, useEffect } from "react";
+import { getTopRatedMovies } from "../../api/movie";
 import GridContainer from "../GridContainer";
+import { useNotification } from '../../hooks';
 
 export default function TopRatedMovies() {
+    const [movies, setMovies] = useState([]);
+    const { updateNotification } = useNotification();
+
+    const fetchMovies = async () => {
+        const { error, movies } = await getTopRatedMovies();
+        if (error) return updateNotification('error', error);
+        setMovies([...movies]);
+    };
+
+    useEffect(() => {
+        fetchMovies();
+    }, []);
+
     return (
         <GridContainer>
-            {Array(5)
-                .fill('')
-                .map((_, index) => {
-                    return <div className='p-5 bg-red-200' key={index}></div>;
-                })
-            }
+            {movies.map((_, index) => {
+                return <div className='p-5 bg-red-200' key={index}></div>;
+            })}
         </GridContainer>
     );
 };
