db48cbe05885435340ccae5e6815546b293a4eae
Carlos Henrique <chtsca2001@gmail.com>
2026-03-04 17:11:40 -0300

    Rendering Up Next Slides

--- src/components/user/HeroSlidShow.jsx
+++ src/components/user/HeroSlidShow.jsx
@@ -26,7 +26,7 @@ export default function HeroSlidShow() {
     };
 
     const startSlideShow = () => {
-        // intervalId = setInterval(handleOnNextClick, 3500);
+        intervalId = setInterval(handleOnNextClick, 3500);
     };
 
     const pauseSlideShow = () => {
@@ -137,7 +137,21 @@ export default function HeroSlidShow() {
             </div>
 
             {/* Up Next Section */}
-            <div className='w-1/5 aspect-video bg-red-300'></div>
+            <div className='w-1/5 space-y-3 px-3'>
+                <h1 className='font-semibold text-2xl text-primary dark:text-white'>
+                    Up Next
+                </h1>
+                {upNext.map(({ poster, id }) => {
+                    return (
+                        <img
+                            key={id}
+                            src={poster}
+                            alt=""
+                            className='aspect-video object-cover rounded'
+                        />
+                    )
+                })}
+            </div>
         </div>
     );
 };
