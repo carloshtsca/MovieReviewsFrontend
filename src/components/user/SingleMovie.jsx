f8dcd4b81288e60dc2a233b18671d54d280da8e4
Carlos Henrique <chtsca2001@gmail.com>
2026-03-05 13:11:21 -0300

    Rendering Movie Info

--- src/components/user/SingleMovie.jsx
+++ src/components/user/SingleMovie.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { getSingleMovie } from "../../api/movie";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useNotification } from "../../hooks";
+import Container from "../Container";
 
 export default function SingleMovie() {
     const { movieId } = useParams();
@@ -23,15 +24,34 @@ export default function SingleMovie() {
         if (movieId) fetchMovie();
     }, [movieId]);
 
-    if (!ready) return <div className="h-screen flex justify-center items-center dark:bg-primary bg-white">
-        <div className="text-light-subtle dark:text-dark-subtle animate-pulse">
-            Please wait
+    if (!ready) return (
+        <div className="h-screen flex justify-center items-center dark:bg-primary bg-white">
+            <div className="text-light-subtle dark:text-dark-subtle animate-pulse">
+                Please wait
+            </div>
         </div>
-    </div>
+    );
+
+    const { id, trailer, poster, title, reviews = {} } = movie;
 
     return (
-        <div>
-            {movie.title}
+        <div className='dark:bg-primary bg-white'>
+            <Container>
+                <video poster={poster} controls src={trailer}></video>
+                <div className='flex justify-between items-center'>
+                    <h1 className='text-4xl text-highlight dark:text-highlight-dark font-semibold py-3'>
+                        {title}
+                    </h1>
+                    <div className=''>
+                        <Link
+                            className='text-highlight dark:text-highlight-dark hover:underline'
+                            to={`/movie/reviews/${id}`}
+                        >
+                            {reviews.reviewCount} Reviews
+                        </Link>
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 };
