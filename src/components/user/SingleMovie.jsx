c24be4287935170ba701301924849d3b71f131b5
Carlos Henrique <chtsca2001@gmail.com>
2026-03-05 18:36:07 -0300

    Rendering Cast Profiles

--- src/components/user/SingleMovie.jsx
+++ src/components/user/SingleMovie.jsx
@@ -41,7 +41,7 @@ export default function SingleMovie() {
             </div>
         </div>
     );
-    console.log(movie)
+
     const { id, trailer, poster, title, storyLine, language, releaseDate, director, type, reviews = {}, writers = [], cast = [], genres = [] } = movie;
 
     return (
@@ -165,6 +165,35 @@ export default function SingleMovie() {
                         <p className='text-highlight dark:text-highlight-dark'>
                             {type}
                         </p>
+                    </div>
+
+                    <div className="mt-5">
+                        <h1 className='text-light-subtle dark:text-dark-subtle font-semibold text-2xl mb-2'>
+                            Cast:
+                        </h1>
+                        <div className='grid grid-cols-10'>
+                            {cast.map(c => {
+                                return (
+                                    <div key={c.profile.id} className='flex flex-col items-center justify-center'>
+                                        <img
+                                            className='w-24 h-24 aspect-square object-cover rounded-full'
+                                            src={c.profile.avatar}
+                                            alt=""
+                                        />
+
+                                        <p className='text-highlight dark:text-highlight-dark hover:underline cursor-pointer'>
+                                            {c.profile.name}
+                                        </p>
+
+                                        <span className='text-light-subtle dark:text-dark-subtle text-sm'>as</span>
+
+                                        <p className='text-light-subtle dark:text-dark-subtle'>
+                                            {c.roleAs}
+                                        </p>
+                                    </div>
+                                )
+                            })}
+                        </div>
                     </div>
 
                 </div>
