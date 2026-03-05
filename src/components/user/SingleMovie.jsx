38e2d5929da3ad3bf740b015defa591d0965d84c
Carlos Henrique <chtsca2001@gmail.com>
2026-03-05 10:33:33 -0300

    Fetching Single Movie

--- src/components/user/SingleMovie.jsx
+++ src/components/user/SingleMovie.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from "react";
+import { getSingleMovie } from "../../api/movie";
+import { useParams } from "react-router-dom";
+import { useNotification } from "../../hooks";
+
 export default function SingleMovie() {
+    const { movieId } = useParams();
+
+    const [ready, setReady] = useState(false);
+    const [movie, setMovie] = useState({});
+
+    const { updateNotification } = useNotification();
+
+    const fetchMovie = async () => {
+        const { error, movie } = await getSingleMovie(movieId);
+        if (error) return updateNotification('error', error);
+        setMovie(movie);
+    };
+
+    useEffect(() => {
+        if (movieId) fetchMovie();
+    }, [movieId])
+
     return (
-        <div>SingleMovie</div>
+        <div>
+            {movie.title}
+        </div>
     );
 };
