b7682b72949e145335814910cef8c6b5cb6738ba
Carlos Henrique <chtsca2001@gmail.com>
2026-03-07 18:33:44 -0300

    Different Icons For Selected Ratings

--- src/components/modals/AddRatingModal.jsx
+++ src/components/modals/AddRatingModal.jsx
@@ -1,23 +1,48 @@
-import { AiOutlineStar } from 'react-icons/ai';
+import { useState } from 'react';
+import { AiOutlineStar, AiFillStar } from 'react-icons/ai';
 import ModalContainer from './ModalContainer';
 import Submit from '../form/Submit';
 
 const ratings = new Array(10).fill('');
 
 export default function AddRatingModal({ }) {
+    const [selectedRatings, setSelectedRatings] = useState([]);
+
+    const handleMouseEnter = (index) => {
+        console.log(index);
+    };
+
     return (
         <ModalContainer visible ignoreContainer>
             <div className="p-5 dark:bg-primary bg-white rounded space-y-3">
-                <div className="text-highlight dark:text-highlight-dark flex items-center">
+                <div className="text-highlight dark:text-highlight-dark flex items-center relative">
                     {ratings.map((_, index) => {
-                        return <AiOutlineStar key={index} size={24} />
+                        return (
+                            <AiOutlineStar
+                                key={index}
+                                onMouseEnter={() => handleMouseEnter(index)}
+                                className='cursor-pointer'
+                                size={24}
+                            />
+                        )
                     })}
+
+                    <div className="absolute flex items-center top-1/2 -translate-y-1/2">
+                        {selectedRatings.map((_, index) => {
+                            return (
+                                <AiFillStar
+                                    key={index}
+                                    onMouseEnter={() => handleMouseEnter(index)}
+                                    className='cursor-pointer'
+                                    size={24}
+                                />
+                            )
+                        })}
+                    </div>
                 </div>
 
                 <textarea className='w-full h-24 border-2 p-2 dark:text-white text-primary 
-                rounded outline-none bg-transparent resize-none'>
-
-                </textarea>
+                rounded outline-none bg-transparent resize-none'></textarea>
 
                 <Submit value='Rate This Movie' />
             </div>
